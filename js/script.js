22d0f34a4f87dacaa3c387362c940caacc5681d1
Sergey Ostapovec <seraphimaion@gmail.com>
2018-10-22 22:55:21 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function () {
+
+  var offset = Math.floor(window.innerHeight / 4);
+
+  new WOW({
+    offset: offset,
+    mobile: false
+  }).init();
+
+
+  // текущая ширина контейнера
+  var containerWidth = 1000;
+  // autowidth
+
+})();
