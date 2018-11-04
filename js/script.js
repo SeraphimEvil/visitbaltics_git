a8567c276aa226d7ee43a01dedd19175e700c193
Sergey Ostapovec <seraphimaion@gmail.com>
2018-11-04 15:38:33 +0300

    test

--- js/script.js
+++ js/script.js
@@ -7,11 +7,30 @@
     mobile: false
   }).init();
 
+  // Дозагрузка изображений
   var imgInstaHover = document.createElement('img');
   imgInstaHover.setAttribute('src', './img/inst_hover.png');
 
   var imgFacebookHover = document.createElement('img');
   imgFacebookHover.setAttribute('src', './img/facebook_hover.png');
+
+  // Scroll intro
+  var currentScrollPosition = window.scrollY;
+  var introElement = document.querySelector('.intro');
+
+  var addIntroScrolledClass = function() {
+    introElement.classList.add('intro--scrolled');
+  };
+
+  if (currentScrollPosition > 0) {
+    addIntroScrolledClass();
+  }
+
+  window.addEventListener('scroll', function (event) {
+    if (!introElement.classList.contains('intro--scrolled')) {
+      addIntroScrolledClass();
+    }
+  })
 
 
 })();
