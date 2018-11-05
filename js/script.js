bfe7495d2b806c16dd5224b9e9cdc537e25b48e5
Sergey Ostapovec <seraphimaion@gmail.com>
2018-11-05 18:22:16 +0300

    upd js

--- js/script.js
+++ js/script.js
@@ -7,13 +7,6 @@
     mobile: false
   }).init();
 
-  // Дозагрузка изображений
-  var imgInstaHover = document.createElement('img');
-  imgInstaHover.setAttribute('src', './img/inst_hover.png');
-
-  var imgFacebookHover = document.createElement('img');
-  imgFacebookHover.setAttribute('src', './img/facebook_hover.png');
-
   // Scroll intro
   // var currentScrollPosition = window.scrollY;
   // var introElement = document.querySelector('.intro');
@@ -31,6 +24,73 @@
   //     addIntroScrolledClass();
   //   }
   // })
+
+  var imgSetAttribute = function (link) {
+    console.log(link)
+
+    document.createElement('img').setAttribute('src', './img/' + link);
+  };
+
+
+  var uploadAllImages = function () {
+    imgSetAttribute('inst_hover.png');
+    imgSetAttribute('facebook_hover.png');
+
+    // page index
+    imgSetAttribute('intro/main_no-logo.jpg');
+    imgSetAttribute('intro/logo.png');
+    imgSetAttribute('tallinn/tallinn.jpg');
+    imgSetAttribute('tallinn/tallinn_note.png');
+    imgSetAttribute('tallinn/tallinn_note.png');
+    imgSetAttribute('tallinn/knocker_signature.png');
+    imgSetAttribute('tallinn/door.jpg');
+    imgSetAttribute('tallinn/lantern.jpg');
+    imgSetAttribute('tallinn/townhall.jpg');
+    imgSetAttribute('tallinn/bottle.jpg');
+    imgSetAttribute('tallinn/ice.jpg');
+    imgSetAttribute('tallinn/cola.jpg');
+    imgSetAttribute('tallinn/coffee.jpg');
+    imgSetAttribute('tallinn/kadriorg.jpg');
+    imgSetAttribute('tallinn/kadriorgflower_bg.jpg');
+    imgSetAttribute('tallinn/palace.jpg');
+    imgSetAttribute('tallinn/viewpoint.jpg');
+    imgSetAttribute('riga/mans.jpg');
+    imgSetAttribute('riga/riga_note.png');
+    imgSetAttribute('riga/sphinx.jpg');
+    imgSetAttribute('riga/blackheads.jpg');
+    imgSetAttribute('riga/price.png');
+    imgSetAttribute('riga/soup_signature.png');
+    imgSetAttribute('riga/peas_signature.png');
+    imgSetAttribute('riga/brothers3.jpg');
+    imgSetAttribute('riga/number.png');
+    imgSetAttribute('riga/balsam.jpg');
+    imgSetAttribute('riga/sprats.jpg');
+    imgSetAttribute('riga/candies.jpg');
+    imgSetAttribute('riga/oldtown.jpg');
+    imgSetAttribute('vilnius/vilnius.jpg');
+    imgSetAttribute('vilnius/vilnius_note.png');
+    imgSetAttribute('vilnius/circle_uzupis.png');
+    imgSetAttribute('vilnius/angel.jpg');
+    imgSetAttribute('vilnius/teapot1.jpg');
+    imgSetAttribute('vilnius/pilies.jpg');
+    imgSetAttribute('vilnius/teapot2.jpg');
+    imgSetAttribute('vilnius/vilnius_architecture.jpg');
+    imgSetAttribute('vilnius/vilnius_view.jpg');
+    imgSetAttribute('vilnius/vilnius_view.jpg');
+    imgSetAttribute('vilnius/balloon.png');
+
+    // page vilnius
+
+
+
+
+
+
+
+
+  };
+
+  uploadAllImages();
 
 
 })();
